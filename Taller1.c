d8146c616a6809256a15aad19152646992a2d618
Matias677 <matias.ortiz.canar@udla.edu.ec>
2026-04-16 21:48:46 -0500

    Corregí bucles for y mejoré ingreso de datos

--- Taller1.c
+++ Taller1.c
@@ -3,9 +3,12 @@ int main (){
     char materias [3][20];
     float notas [3][5];
     for (int i=0; i<3; i++){
+        printf("Ingrese el nombre de las  materias:\n");
         scanf("%s", materias[i]);
+        for(int j=0; j<5; j++){
+        printf("Ingrese la nota:\n");
         scanf("%f", &notas[i][j]); 
+        }
     }
-
     return 0;
 }

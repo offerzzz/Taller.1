2eb5406faad5e7ae6509a2f42e153ad537337766
Offer Peñafiel <offer.penafiel@udla.edu.ec>
2026-04-19 21:20:41 -0500

    Corrigiendo erroes y terminando el codigo

--- Taller1.c
+++ Taller1.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 int main (){
+    int numestudiantes=5;
+    int nummaterias=3;
     char materias [3][20];
-    float notas [3][5];
+    float notas [5][3];
     for (int i=0; i<3; i++){
-        printf("Ingrese el nombre de las  materias %d: ", i++);
+        printf("Ingrese el nombre de las  materias:\n");
         scanf("%s", materias[i]);
-        for(int j=0; j<5; j++);
-        printf("Ingrese la nota %d: ", j++);
+        for(int j=0; j<5; j++){
+        printf("Ingrese la nota:\n");
         scanf("%f", &notas[i][j]); 
+        while (notas[i][j] <0 || notas[i][j] >10){
+            printf("nota invalida ingrese otra: ");
+            scanf("%f", &notas[i][j]);
+        }
+        }
     }
+    return 0;
+
+    
 
 }
